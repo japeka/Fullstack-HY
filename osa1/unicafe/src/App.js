ed5a1b30c924a762e1212a953d19d707ed2ba1b9
Janne Kalliokulju <janne.kalliokulju@gmail.com>
2022-08-04 10:45:43 +0300

    methods implementations added

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -31,15 +31,15 @@ const Button = (props) => (
 )
 
 const App = () => {
-  const [good, setGood] = useState(5)
-  const [neutral, setNeutral] = useState(2)
-  const [bad, setBad] = useState(1)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
   return (
     <div>
         <h1>give feedback</h1>
-        <Button handleClick={()=>{}} text="good" />
-        <Button handleClick={()=>{}} text="neutral" />
-        <Button handleClick={()=>{}} text="bad" />
+        <Button handleClick={()=>setGood(good+1)} text="good" />
+        <Button handleClick={()=>setNeutral(neutral+1)} text="neutral" />
+        <Button handleClick={()=>setBad(bad+1)} text="bad" />
         <Statistics 
           good={good} 
           neutral={neutral} 
