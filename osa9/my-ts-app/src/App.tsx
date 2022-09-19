11e0652e1dfd216551b0c0c488f750541e796419
Janne Kalliokulju <janne.kalliokulju@gmail.com>
2022-09-19 23:10:13 +0300

    Exercise 9.15 added

--- osa9/my-ts-app/src/App.tsx
+++ osa9/my-ts-app/src/App.tsx
@@ -1,30 +1,62 @@
 import React from "react";
 
+// new types
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CoursePartWithDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CoursePartWithDescription {
+  type: "normal";
+}
+
+interface CourseNormalSpecial extends CoursePartWithDescription {
+  type: "special";
+  requirements: string[];
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartWithDescription {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart =
+  | CourseNormalPart
+  | CourseProjectPart
+  | CourseSubmissionPart
+  | CourseNormalSpecial;
+
+const Part = ({ part }: { part: CoursePart }): JSX.Element => {
+  return (
+    part && <li>{Object.values(part).map((po) => `--${po.join(",")}`)}</li>
+  );
+};
+
 const Header = ({ name }: { name: string }): JSX.Element => {
   return <h1>{name}</h1>;
 };
 
-const Content = ({
-  parts,
-}: {
-  parts: { name: string; exerciseCount: number }[];
-}): JSX.Element => {
+const Content = ({ parts }: { parts: CoursePart[] }): JSX.Element => {
   return (
-    <div>
+    <ul>
       {parts.map((p, i) => (
-        <p key={i}>
-          {p.name} {p.exerciseCount}
-        </p>
+        <Part key={i} part={p} />
       ))}
-    </div>
+    </ul>
   );
 };
 
-const Total = ({
-  parts,
-}: {
-  parts: { name: string; exerciseCount: number }[];
-}): JSX.Element => {
+const Total = ({ parts }: { parts: CoursePart[] }): JSX.Element => {
   return (
     <p>
       Number of exercises{" "}
@@ -33,20 +65,47 @@ const Total = ({
   );
 };
 
+// const assertNever = (value: never): never => {
+//   throw new Error(
+//     `Unhandled discriminated union member: ${JSON.stringify(value)}`
+//   );
+// };
+
 const App = (): JSX.Element => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is the easy course part",
+      type: "normal",
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the hard course part",
+      type: "normal",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special",
     },
   ];
   return (
